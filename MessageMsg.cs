9aaa19ae6fb8f1a4bf6b4312a346b3b4f4d75332
Joanajua <J.Ayan@ucas.ac.uk>
2020-01-23 17:00:18 +0000

    Averages done and prit to external files

--- MessageMsg.cs
+++ MessageMsg.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Gasmon
 {
     public class MessageMsg
-    
+
     {
         public string locationId { get; set; }
         public string eventId { get; set; }
         public double value { get; set; }
         public long timestamp { get; set; }
-        
+
     }
+    
 }
--- Program.cs
+++ Program.cs
@@ -20,9 +20,57 @@ namespace Gasmon
         public static async Task Main(string[] args)
         {
             ///External Credentials
-            
+
             AWSCredentials credentials = GetGasmonCredentials();
             AmazonS3Client amazonS3Client = new AmazonS3Client(credentials, RegionEndpoint.EUWest1);
+            Dictionary<string, JsonFile> locationsById = await GetJson(amazonS3Client);
+
+            var sns = new AmazonSimpleNotificationServiceClient(credentials, RegionEndpoint.EUWest1);
+            var sqs = new AmazonSQSClient(credentials, RegionEndpoint.EUWest1);
+            var myTopicArn = "arn:aws:sns:eu-west-1:552908040772:EventProcessing-UCAS2-snsTopicSensorDataPart1-OVN4WSEGUZ58";
+
+            var myQueueName = "Joana-GasMon-" + Guid.NewGuid();
+            var myQueueUrl = (await sqs.CreateQueueAsync(myQueueName)).QueueUrl;
+
+            await sns.SubscribeQueueAsync(myTopicArn, sqs, myQueueUrl);
+
+
+
+            
+
+
+            DateTime initTime = DateTime.Now;
+            Console.WriteLine("The Initial time is: {0}", initTime);
+            DateTime finalTime = initTime.AddMinutes(6);
+           
+
+            for (int i = 0; i < 6; i++)
+            {
+                Dictionary<string, MessageMsg> dictionary = new Dictionary<string, MessageMsg>();
+                DateTime loopTime = initTime.AddMinutes(i + 1);
+                do
+                {
+                    List<Amazon.SQS.Model.Message> messages = (await sqs.ReceiveMessageAsync(new ReceiveMessageRequest(myQueueUrl)
+                    { WaitTimeSeconds = 1 })).Messages;
+                    LoopingMessages(locationsById, dictionary, initTime, finalTime, messages);
+                }
+                while (DateTime.Now < loopTime);
+
+                var fileName = "";
+                fileName = WritingFile(dictionary, i, fileName, initTime);
+   
+            }
+
+
+            Console.WriteLine("The Final time is: {0}", finalTime);
+
+
+            Console.ReadKey();
+
+        }
+
+        private static async Task<Dictionary<string, JsonFile>> GetJson(AmazonS3Client amazonS3Client)
+        {
             List<JsonFile> myJsonDeserialize = new List<JsonFile>();
             Dictionary<string, JsonFile> locationsById;
 
@@ -32,69 +80,42 @@ namespace Gasmon
                 {
                     string contents = reader.ReadToEnd();
 
-                    //A JsonFile created. Making a list to put the jsonFile information into it.
                     myJsonDeserialize = JsonConvert.DeserializeObject<List<JsonFile>>(contents);
                     locationsById = myJsonDeserialize.ToDictionary(location => location.id);
 
                     foreach (JsonFile json in myJsonDeserialize)
-                    { 
+                    {
                         Console.WriteLine("x: {0}, y: {1}, id: {2}", json.x, json.y, json.id);
                     }
                 }
             }
 
-            //Part of the AWSSKD.SimpleNotificationService NuGet package.
-            var sns = new AmazonSimpleNotificationServiceClient(credentials, RegionEndpoint.EUWest1);
-            //Part of the AWSSDK.SQS NuGet package.
-            var sqs = new AmazonSQSClient(credentials, RegionEndpoint.EUWest1);
-            //The topic ARN supplied
-            var myTopicArn = "arn:aws:sns:eu-west-1:552908040772:EventProcessing-UCAS2-snsTopicSensorDataPart1-OVN4WSEGUZ58";
-            //// Create a new queue. You can't reuse a queue name within 60 seconds so use a GUID
-            // to generate a new random name.
-            var myQueueName = "Joana-GasMon-" + Guid.NewGuid();
-            var myQueueUrl = (await sqs.CreateQueueAsync(myQueueName)).QueueUrl;
-            //In order for a queue to receive messages from a topic,
-            //it needs to be subscribed and also needs a custom security
-            //policy to allow the topic to deliver messages to the queue
-            await sns.SubscribeQueueAsync(myTopicArn, sqs, myQueueUrl);
+            return locationsById;
+        }
 
-
-
-            Dictionary<string, MessageMsg> EventsId = new Dictionary<string, MessageMsg>();
-            
-
-
-            DateTime initialTime = DateTime.Now;
-            DateTime finalTime = initialTime.AddMinutes(6);
-            TimeSpan totalTime = finalTime - initialTime;
-            Console.WriteLine("The Initial time is: {0}", initialTime);
-            
-
-            do
-            {
-                List<Amazon.SQS.Model.Message> messages = (await sqs.ReceiveMessageAsync(new ReceiveMessageRequest(myQueueUrl)
-                { WaitTimeSeconds = 1 })).Messages;
-                CollectMessages(locationsById, EventsId, initialTime, finalTime, messages);
-
-            }
-            while (DateTime.Now < finalTime);
-
-            ///////////////////////////////////////////////////////////////////////////////////////
-            var listName="file1";
+        private static string WritingFile(Dictionary<string, MessageMsg> dictionary, int i, string fileName, DateTime initTime)
+        {
             try
             {
-                string path = @"C:\Work\Training\12.Gasmon\Gasmon\MessagesFiles\" + listName+ ".txt";
+                fileName = "file" + (i + 1);
+                string path = @"C:\Work\Training\12.Gasmon\Gasmon\MessagesFiles\" + fileName + ".txt";
                 using (StreamWriter file = File.CreateText(path))
                 {
-                    foreach(KeyValuePair<string, MessageMsg> keyValuePair in EventsId)
+                    foreach (var group in dictionary.Values.GroupBy(l => l.locationId))
                     {
-                        file.Write("EventId: {0}\nLocationId: {1}\nValue: {2}\nTimestamp: {3}\n\n",
-                            keyValuePair.Key, keyValuePair.Value.locationId, keyValuePair.Value.value, 
-                            keyValuePair.Value.timestamp);
+                        var locationId = group.Key;
+                        double average = group.Average(l => l.value);
+                        file.Write("LocationId: {0}\nValueAverage: {1} \n\n",
+                            locationId, average);
                     }
+                    //foreach (KeyValuePair<string, MessageMsg> keyValuePair in dictionary)
+                    //{
+                    //    file.Write("LocationId: {0}\nValue: {1}\nEventId: {2}\n\n",
+                    //        keyValuePair.Key, keyValuePair.Value.locationId, keyValuePair.Value.value);
+                    //}
 
-                    file.Write("\nTotal time passed is: {0}\n\n" +
-                        "Total amount of messages recived was: {1}", totalTime, EventsId.Count);
+                    file.Write("\nInitial time : {0}\n\n" +
+                        "Total amount of messages recived was: {1}", initTime, dictionary.Count);
                 }
             }
             catch (Exception Ex)
@@ -102,16 +123,13 @@ namespace Gasmon
                 Console.WriteLine(Ex.ToString());
             }
 
-            
-            Console.WriteLine("The Final time is: {0}", finalTime);
-            
-
-            Console.ReadKey();
-
+            return fileName;
         }
 
-        private static void CollectMessages(Dictionary<string, JsonFile> locationsById, Dictionary<string, MessageMsg> EventsId, DateTime initialTime, DateTime finalTime, List<Amazon.SQS.Model.Message> messages)
+        private static void LoopingMessages(Dictionary<string, JsonFile> locationsById, Dictionary<string, MessageMsg>
+            dictionary, DateTime initialTime, DateTime finalTime, List<Message> messages)
         {
+
             foreach (var message in messages)
             {
 
@@ -124,21 +142,23 @@ namespace Gasmon
                 Console.WriteLine("The MESSAGE is: \n locationId: " + snsMessageDeserialized.locationId +
                     "\n eventId: " + snsMessageDeserialized.eventId + "\n value: "
                     + snsMessageDeserialized.value + "\n timestamp: " + snsMessageDeserialized.timestamp
-                     + "\n timestamp: " + timestampDate);
+                    + "\n timestamp: " + timestampDate);
 
 
-                if (!EventsId.ContainsKey(snsMessageDeserialized.eventId) &&
-                    locationsById.ContainsKey(snsMessageDeserialized.locationId))
+                if (!dictionary.ContainsKey(snsMessageDeserialized.eventId) &&
+                locationsById.ContainsKey(snsMessageDeserialized.locationId))
                 {
-                    //Checking messages are not doubled
-                    //making sure just taking messages from our sensors
+
                     if (timestampDate >= initialTime && timestampDate <= finalTime)
-                        //taking all messages sended during our timespam (1min) even if they arrive late
                     {
-                        EventsId.TryAdd(snsMessageDeserialized.eventId, snsMessageDeserialized);
+                        dictionary.TryAdd(snsMessageDeserialized.eventId, snsMessageDeserialized);
+                        
                     }
                 }
             }
+            //////////////////////////////////////////////////
+            
+
         }
 
         private static DateTime epoch2date(long epoch)
